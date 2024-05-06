92113fa3e739aca2a42502ad40c2fcdee164fbce
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-06 18:15:56 -0300

    Deleção de usuario com delete

--- src/main/java/br/com/compassuol/workshopmongo/services/UserService.java
+++ src/main/java/br/com/compassuol/workshopmongo/services/UserService.java
@@ -30,6 +30,12 @@ public class UserService {
 		return user;
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+		
+	}
+	
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}

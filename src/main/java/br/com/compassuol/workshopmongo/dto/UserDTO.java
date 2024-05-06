3ec7474aabdedcbf82dcbf1e343dcdb4efc819af
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-06 16:50:26 -0300

    Usando padrão DTO para retornar usuários

---
+++ src/main/java/br/com/compassuol/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,55 @@
+package br.com.compassuol.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+
+import br.com.compassuol.workshopmongo.domain.User;
+
+public class UserDTO implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+
+	
+	private String id;
+	
+	private String name, email;
+
+	public UserDTO() {
+		super();
+	}
+
+	public UserDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}

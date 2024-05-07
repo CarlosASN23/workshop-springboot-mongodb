151b9260bfb9cc224d657872608991e851cfc00b
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-07 15:16:34 -0300

    consulta simples mongo query

--- src/main/java/br/com/compassuol/workshopmongo/repository/Postrepository.java
+++ src/main/java/br/com/compassuol/workshopmongo/repository/Postrepository.java
@@ -1,11 +1,17 @@
 package br.com.compassuol.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.compassuol.workshopmongo.domain.Post;
 
 @Repository
 public interface Postrepository extends MongoRepository<Post,String>{
-
+	
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/br/com/compassuol/workshopmongo/resource/PostResource.java
+++ src/main/java/br/com/compassuol/workshopmongo/resource/PostResource.java
@@ -1,23 +1,18 @@
 package br.com.compassuol.workshopmongo.resource;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.compassuol.workshopmongo.domain.Post;
-import br.com.compassuol.workshopmongo.domain.User;
-import br.com.compassuol.workshopmongo.dto.UserDTO;
+import br.com.compassuol.workshopmongo.resource.util.URL;
 import br.com.compassuol.workshopmongo.services.PostService;
-import br.com.compassuol.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -26,10 +21,20 @@ public class PostResource {
 	@Autowired
 	private PostService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+		
+	}
+	
+	@RequestMapping(value = "/titlesearch",method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "")String text){
+		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		
+		return ResponseEntity.ok().body(list);
 		
 	}
 	

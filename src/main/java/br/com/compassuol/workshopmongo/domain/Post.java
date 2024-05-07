010396294939bb783e3457087ba10f53562f2ee8
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-07 14:52:52 -0300

    adicionando comentários aos posts

--- src/main/java/br/com/compassuol/workshopmongo/domain/Post.java
+++ src/main/java/br/com/compassuol/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package br.com.compassuol.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import br.com.compassuol.workshopmongo.dto.AuthorDTO;
+import br.com.compassuol.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -21,6 +24,7 @@ public class Post implements Serializable{
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -66,6 +70,22 @@ public class Post implements Serializable{
 	public void setBody(String body) {
 		this.body = body;
 	}
+	
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -83,15 +103,4 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	public AuthorDTO getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(AuthorDTO author) {
-		this.author = author;
-	}
-	
-	
-
 }

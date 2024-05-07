2f41cd92b3afd15862bc11767f42ab326fee365e
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-05-07 14:22:24 -0300

    criando post aninhados

--- src/main/java/br/com/compassuol/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/compassuol/workshopmongo/config/Instantiation.java
@@ -1,31 +1,47 @@
 package br.com.compassuol.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.compassuol.workshopmongo.domain.Post;
 import br.com.compassuol.workshopmongo.domain.User;
+import br.com.compassuol.workshopmongo.repository.Postrepository;
 import br.com.compassuol.workshopmongo.repository.UserRepository;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository userRepository;
-	
-	@Override
-	public void run(String... args) throws Exception {
+	private UserRepository userReposiroty;
 
-		userRepository.deleteAll();
+	@Autowired
+	private Postrepository postReposiroty;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
-		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",maria);
+
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
+
 	}
 
 }
